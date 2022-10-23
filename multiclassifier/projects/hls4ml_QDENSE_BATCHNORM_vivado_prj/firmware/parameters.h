3579592ca101141fc82fff300f91b3229c4ec576
Giuseppe Di Guglielmo <gdg@fnal.gov>
2022-10-22 21:11:57 -0700

    Add project w/ qdense batchnorm folding

---
+++ multiclassifier/projects/hls4ml_QDENSE_BATCHNORM_vivado_prj/firmware/parameters.h
@@ -0,0 +1,95 @@
+#ifndef PARAMETERS_H_
+#define PARAMETERS_H_
+
+#include "ap_int.h"
+#include "ap_fixed.h"
+
+#include "nnet_utils/nnet_helpers.h"
+//hls-fpga-machine-learning insert includes
+#include "nnet_utils/nnet_activation.h"
+#include "nnet_utils/nnet_activation_stream.h"
+#include "nnet_utils/nnet_dense.h"
+#include "nnet_utils/nnet_dense_compressed.h"
+#include "nnet_utils/nnet_dense_stream.h"
+ 
+//hls-fpga-machine-learning insert weights
+#include "weights/w2.h"
+#include "weights/b2.h"
+#include "weights/w5.h"
+#include "weights/b5.h"
+
+//hls-fpga-machine-learning insert layer-config
+// dense1
+struct config2 : nnet::dense_config {
+    static const unsigned n_in = N_INPUT_1_1;
+    static const unsigned n_out = N_LAYER_2;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 254;
+    static const unsigned n_nonzeros = 1538;
+    static const bool merged_relu = false;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef bias2_t bias_t;
+    typedef weight2_t weight_t;
+    typedef ap_uint<1> index_t;
+    //typedef layer2_t:: value_type out_t;
+    typedef layer2_t out_t;
+    template<class x_T, class y_T, class res_T>
+    using product = nnet::product::mult<x_T, y_T, res_T>;
+};
+
+// dense1_linear
+struct linear_config3 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_2;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+// relu1
+struct relu_config4 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_2;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+// dense2
+struct config6 : nnet::dense_config {
+    static const unsigned n_in = N_LAYER_2;
+    static const unsigned n_out = N_LAYER_6;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 13;
+    static const unsigned n_nonzeros = 371;
+    static const bool merged_relu = true;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef bias5_t bias_t;
+    typedef weight5_t weight_t;
+    typedef ap_uint<1> index_t;
+    //typedef layer6_t:: value_type out_t;
+    typedef layer6_t out_t;
+    template<class x_T, class y_T, class res_T>
+    using product = nnet::product::mult<x_T, y_T, res_T>;
+};
+
+// softmax
+struct softmax_config7 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_6;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    static const unsigned axis = -1;
+    static const nnet::softmax_implementation implementation = nnet::softmax_implementation::latency;
+    typedef ap_fixed<18,8> exp_table_t;
+    typedef ap_fixed<18,4> inv_table_t;
+};
+
+
+#endif
---
+++ multiclassifier/projects/hls4ml_QDENSE_BATCHNORM_vivado_prj/firmware/weights/b5.h
@@ -0,0 +1,15 @@
+//Numpy array shape [3]
+//Min -0.250000000000
+//Max 0.187500000000
+//Number of zeros 0
+
+#ifndef B5_H_
+#define B5_H_
+
+#ifndef __SYNTHESIS__
+bias5_t b5[3];
+#else
+bias5_t b5[3] = {0.1250, -0.2500, 0.1875};
+#endif
+
+#endif
